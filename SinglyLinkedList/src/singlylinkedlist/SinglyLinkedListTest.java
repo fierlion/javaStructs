7fbc7b153a4cd90a40de0e0013fbfe255edc0d00
rfoote <fierlion@gmail.com>
2014-11-13 13:21:48 -0800

    Added tests for sorted list

--- SinglyLinkedList/src/singlylinkedlist/SinglyLinkedListTest.java
+++ SinglyLinkedList/src/singlylinkedlist/SinglyLinkedListTest.java
@@ -12,13 +12,17 @@ package singlylinkedlist;
 public class SinglyLinkedListTest {
     public static void main(String [] args) {
 	SinglyLinkedList list1 = new SinglyLinkedList();
-	list1.add(0);
-	list1.add(1);
-	list1.add(2);
-	
+	list1.addSorted(5);
+	list1.addSorted(0);
+	list1.addSorted(1);
+	list1.addSorted(2);
+	list1.addSorted(1);
+
 	System.out.println(list1.toString());
 	list1.add(1,10);
 	list1.add(0,20);
+	System.out.println(list1.toString());
+	list1.remove(4);
 	System.out.println(list1.toString());
 	
     }
