16d5aac9364408e602bfc4c62ac617f40556dbf2
rfoote <fierlion@gmail.com>
2014-11-14 15:19:51 -0800

    Initial TreeNode class commit

---
+++ BinaryTree/src/binarytree/TreeNode.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarytree;
+
+/**
+ *
+ * @author rfoote
+ */
+public class TreeNode {
+    public int data;
+    public TreeNode left;
+    public TreeNode right;
+    
+    public TreeNode(int dataIn) {
+	this(dataIn, null, null);
+    }
+    
+    public TreeNode(int dataIn, TreeNode leftIn, TreeNode rightIn) {
+	data = dataIn;
+	left = leftIn;
+	right = rightIn;
+    }
+}
