91ed0fd620c0676864ac56bfdd7fbcc690710a4f
rfoote <fierlion@gmail.com>
2014-11-14 15:38:01 -0800

    Added TreeNode

--- BinaryTree/src/binarytree/BinaryTree.java
+++ BinaryTree/src/binarytree/BinaryTree.java
@@ -11,11 +11,4 @@ package binarytree;
  */
 public class BinaryTree {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-	// TODO code application logic here
-    }
-    
 }
