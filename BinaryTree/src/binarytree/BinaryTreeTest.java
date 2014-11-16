d0657cf873eeac18868dfb980b4f8bd6db6985f6
rayfoote <rayfoote@rays-air>
2014-11-15 17:51:03 -0800

    Added tests for buildTree() and all print() methods

--- BinaryTree/src/binarytree/BinaryTreeTest.java
+++ BinaryTree/src/binarytree/BinaryTreeTest.java
@@ -7,8 +7,21 @@ package binarytree;
 
 /**
  *
- * @author rfoote
+ * @author rfoote/ Reges/Stepp
  */
 public class BinaryTreeTest {
-    
+    public static void main(String [] args) {
+        BinaryTree tree1 = new BinaryTree(14);
+        System.out.print("preorder: ");
+        tree1.printPreorder(tree1.getRoot());
+        System.out.println();
+        System.out.print("inorder: ");
+        tree1.printInorder(tree1.getRoot());
+        System.out.println();
+        System.out.print("postorder: ");
+        tree1.printPostorder(tree1.getRoot());
+        System.out.println();
+        System.out.println("sideways: ");
+        tree1.printSideways(tree1.getRoot(), 0);
+    }
 }
