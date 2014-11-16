1f38d45321995f7883b202ab6ae2cb84235d5e3d
rayfoote <rayfoote@rays-air>
2014-11-15 17:50:31 -0800

    Added print methods (pre, in post-order, sideways) and simple private build method to build tree from 1 through max (inclusive)

--- BinaryTree/src/binarytree/BinaryTree.java
+++ BinaryTree/src/binarytree/BinaryTree.java
@@ -7,8 +7,74 @@ package binarytree;
 
 /**
  *
- * @author rfoote
+ * @author rfoote/ Reges/Stepp
  */
-public class BinaryTree {
+public class BinaryTree implements Tree{
+    private TreeNode root;
+    
+    public TreeNode getRoot() {
+        return root;
+    }
+    
+    public BinaryTree(int max) {
+        if (max > 0) {
+            root = buildTree(1, max);
+        } 
+        else {
+            throw new IllegalArgumentException("max: " + max);
+        }
+    }
 
+    //fills tree with numbers 1 through max (inclusive) 
+    private TreeNode buildTree(int n, int max) {
+        if (n > max) {
+            return null;
+        } 
+        else {
+            return new TreeNode(n, buildTree(2*n, max), buildTree(2*n+1, max));
+        }
+    }
+    
+    @Override
+    public void printPreorder(TreeNode rootIn) {
+        if (rootIn != null) {
+            System.out.print(" " + rootIn.data);
+            printPreorder(rootIn.left);
+            printPreorder(rootIn.right);
+        }
+    }
+
+    @Override
+    public void printInorder(TreeNode rootIn) {
+        if (rootIn != null) {
+            printInorder(rootIn.left);
+            System.out.print(" " + rootIn.data);
+            printInorder(rootIn.right);
+        }
+      
+    }
+
+    @Override
+    public void printPostorder(TreeNode rootIn) {
+        if (rootIn != null) {
+            printPostorder(rootIn.left);
+            printPostorder(rootIn.right);
+            System.out.print(" " + rootIn.data);
+        }
+
+    }
+
+    @Override
+    public void printSideways(TreeNode rootIn, int level) {
+        if (rootIn != null) {
+            printSideways(rootIn.right, level + 1);
+            for (int i = 0; i < level; i++) {
+                System.out.print("   ");
+            }
+            System.out.println(rootIn.data);
+            printSideways(rootIn.left, level + 1);
+        }
+    }
+    
+    
 }
