1891aecfa9ea98f0d0fb12085e0ef670d96d2a96
rfoote <fierlion@gmail.com>
2014-11-18 13:58:13 -0800

    Changed name and added tests for Binary Search iterative for int and recursive for String

--- SortingAlgos/src/searchingalgos/SearchingAlgos.java
+++ SortingAlgos/src/searchingalgos/SearchingAlgos.java
@@ -3,24 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sortingalgos;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+package searchingalgos;
 
 /**
  *
  * @author rfoote
  */
-public class SortingAlgos {
+public class SearchingAlgos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-	String test1 = "String";
-	String test2 = "String2";
 	String [] ranks = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
 	String [] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
 	Integer [] numArr = {0,1,2,3,4,5,6,7,8,9,200,9,-23};
@@ -38,15 +32,22 @@ public class SortingAlgos {
 	intShuf.shuffle(numArr);
 	System.out.println(numArr[0]);
 	
+	//binary search int test
 	BinarySearchInt bsi = new BinarySearchInt();
 	int intAnsIndx = bsi.bsi(intArr, 5);
 	System.out.println(intAnsIndx);
 	
+	
+	//binary search String (recursive) tests
 	BinarySearchString bss = new BinarySearchString();
 	int strAnsIndx = bss.bss(suits, "Diamonds", 0, suits.length - 1);
 	int strAnsIndxBad = bss.bss(suits, "Dogs", 0, suits.length - 1);
-	System.out.println(strAnsIndx);
-	System.out.println(strAnsIndxBad);
+	System.out.println("Expected behavior:");
+	System.out.println("Index of String 'Diamonds' in String [] suits (should be 1): " + strAnsIndx);
+	System.out.println("Index of String 'Dogs' in String [] suits (should be -1): " + strAnsIndxBad);
+	
+	
+	
     }
     
 }
